1e6115f1e74392e299d6c44766c9cd76f40d27c2
Sanket-Programmer <sanketmudulicoder1@gmail.com>
2026-04-04 22:27:22 +0530

    fix db.js

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,12 +1,16 @@
-import 'dotenv/config'; 
-import { Pool } from "pg";
+import "dotenv/config";
+import pkg from "pg";
+const { Pool } = pkg;
 
 const pool = new Pool({
   user: process.env.DB_USER,
   host: process.env.DB_HOST,
   database: process.env.DB_NAME,
   password: process.env.DB_PASSWORD,
-  port: process.env.DB_PORT,
+  port: Number(process.env.DB_PORT),
+  ssl: {
+    rejectUnauthorized: false,
+  },
 });
 
 export default pool;
